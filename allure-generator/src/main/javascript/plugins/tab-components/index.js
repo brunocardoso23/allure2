2f31069f8cfb19eae145610afaf8675eeb20e898
Bruno Henrique Cardoso <bruno.cardoso@softexpert.com>
2019-06-25 11:47:48 -0300

    Correção

--- allure-generator/src/main/javascript/plugins/tab-components/index.js
+++ allure-generator/src/main/javascript/plugins/tab-components/index.js
@@ -1,7 +1,7 @@
 import graphCom from './graphCom';
 
 allure.api.addTab('graphCom', {
-    title: 'Suites', icon: 'fa fa-pie-chart',
+    title: 'Components', icon: 'fa fa-pie-chart',
     route: 'graphCom',
     onEnter: () => new graphCom()
 });
